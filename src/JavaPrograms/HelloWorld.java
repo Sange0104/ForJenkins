48250d965b69a0f8413ab2fda8f50f3d3ed3edf7
Sangeetha <Sangeetha@LAPTOP-K6OMI7N5.myfiosgateway.com>
2022-06-18 19:48:59 -0400

    Checking trigger

--- src/JavaPrograms/HelloWorld.java
+++ src/JavaPrograms/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Sangeetha is an excellent Tester");
+		System.out.println("She is doing great job");
 	}
 
 }
